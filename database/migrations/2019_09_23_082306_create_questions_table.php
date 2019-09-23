90fea716ec374bc93e3e2712f97c58052d8ad663
Aufal Marom <aufalmarom23@gmail.com>
2019-09-23 15:47:51 +0700

    question model and migration

---
+++ database/migrations/2019_09_23_082306_create_questions_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('body');
+            $table->integer('views')->default(0);
+            $table->integer('answers')->default(0);
+            $table->integer('vote')->default(0);
+            $table->bigInteger('best_answer_id')->nullable()->unsigned();
+            $table->bigInteger('user_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
