8d777e431b1ea4fe37df861e16f83afe5fb1c8a7
aufalmarom <aufalmarom23@gmail.com>
2019-12-22 21:30:59 +0700

    display all questions with pagination

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::resource('questions', 'QuestionsController');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
